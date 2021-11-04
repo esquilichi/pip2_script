25462830743fabfb5bc435133816889dd4504253
Ismael Esquilichi <67276772+esquilichi@users.noreply.github.com>
2021-11-04 11:55:59 +0100

    Add files via upload

---
+++ get-pip2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+url="https://bootstrap.pypa.io/pip/2.7/get-pip.py"
+
+# Descargar script en /temp
+
+wget -P /temp $url
+
+if [ $? -eq 0 ]; then
+    # Ejecutar script
+    echo "[*] Ejecutando script..."
+    python2.7 /temp/get-pip.py
+    if [ $? -eq 0 ]; then
+        echo "Script ejecutado correctamente"
+        echo "Acuérdate de agregar /home/$USER/.local/bin"
+    else
+        echo "Error al ejecutar script"
+    fi
+else
+    echo "Error al descargar script"
+fi
